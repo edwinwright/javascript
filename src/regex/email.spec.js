8ef9b6f44bee85b074d8bd70541e822fd70080e8
Ed Wright <edwinwright@gmail.com>
2016-04-22 08:39:55 +0100

    Added custom matcher toPassRegex()

--- src/regex/email.spec.js
+++ src/regex/email.spec.js
@@ -1,10 +1,31 @@
 import {email} from './email.js';
 
 // TODO: Check these emails.
-// TODO: Rewrite so that the error messages return the email address in the console.
 
 /** @test {email} */
-describe('email() regex', function() {
+fdescribe('email() regex', function() {
+
+  beforeAll(function() {
+
+    jasmine.addMatchers({
+      toPassRegex: function() {
+        return {
+          compare: function(actual, regex) {
+            var result = {};
+            result.pass = regex.test(actual);
+
+            if (result.pass) {
+              result.message = 'Expected ' + actual + ' not to pass regex test.';
+            } else {
+              result.message = 'Expected ' + actual + ' to pass regex test.';
+            }
+            return result;
+          }
+        };
+      }
+    });
+
+  });
 
   // Test cases for success
   it('should return true when the value is not an email', function() {
@@ -15,8 +36,8 @@ describe('email() regex', function() {
       'email@subdomain.example.com',
       'firstname+lastname@example.com',
       'email@123.123.123.123',
-      'email@[123.123.123.123]', // false
-      '“email”@example.com', // false
+      'email@[123.123.123.123]',
+      '“email”@example.com',
       '1234567890@example.com',
       'email@example-one.com',
       '_______@example.com',
@@ -24,14 +45,14 @@ describe('email() regex', function() {
       'email@example.museum',
       'email@example.co.jp',
       'firstname-lastname@example.com',
-      'much.“more\ unusual”@example.com', // false
-      'very.unusual.“@”.unusual.com@example.com', // false
-      'very.“(),:;<>[]”.VERY.“very@\\ "very”.unusual@strange.example.com', // false
+      'much.“more\ unusual”@example.com',
+      'very.unusual.“@”.unusual.com@example.com',
+      'very.“(),:;<>[]”.VERY.“very@\\ "very”.unusual@strange.example.com',
     ];
 
     validValues.forEach(value => {
       let regex = email();
-      expect(regex.test(value)).toBe(true);
+      expect(value).toPassRegex(regex);
     });
 
   });
@@ -46,17 +67,17 @@ describe('email() regex', function() {
       'Joe Smith <email@example.com>',
       'email.example.com',
       'email@example@example.com',
-      '.email@example.com', // true
-      'email.@example.com', // true
-      'email..email@example.com', // true
+      '.email@example.com',
+      'email.@example.com',
+      'email..email@example.com',
       'あいうえお@example.com',
       'email@example.com (Joe Smith)',
-      'email@example', // true
+      'email@example',
       'email@-example.com',
-      'email@example.web', // true
-      'email@111.222.333.44444', // true
+      'email@example.web',
+      'email@111.222.333.44444',
       'email@example..com',
-      'Abc..123@example.com', // true
+      'Abc..123@example.com',
       '“(),:;<>[\]@example.com',
       'just"not"right@example.com',
       'this\ is"really"not\allowed@example.com',
@@ -64,7 +85,7 @@ describe('email() regex', function() {
 
     invalidValues.forEach(value => {
       let regex = email();
-      expect(email().test(value)).toBe(false);
+      expect(value).not.toPassRegex(regex);
     });
 
   });
